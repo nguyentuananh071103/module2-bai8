589830315511f743058289dabf8b8380af9f7232
nguyentuananh071103 <nguyenngocbao13579@gmail.com>
2021-10-18 16:55:07 +0700

    update

--- trien khai cac phuong thuc ArrayList/MyList.php
+++ trien khai cac phuong thuc ArrayList/MyList.php
@@ -5,7 +5,7 @@ class MyList
     public array $elements;
     public function __construct($size, $arr="")
     {
-        if (is_list($arr)){
+        if (is_array($arr)){
 
             $this->elements = $arr;
         }else{
@@ -14,17 +14,42 @@ class MyList
     }
     public function insert($index, $obj)
     {
-
+        array_splice($this->elements, $index, 0, $obj);
     }
     public function add($obj)
     {
-        array_push($this->myList, $obj);
+        array_push($this->elements, $obj);
     }
     public function remove($index)
     {
-        if(array_key_exists($index, $this->list));
-        {
-            unset(($this->list[$index]));
+        if ($this->isInteger($index)) {
+            $newArrayList = [];
+            for ($i = 0; $i < $this->size(); $i++) {
+                if ($index != $i) $newArrayList[] = $this->get($i);
+            }
+            $this->elements = $newArrayList;
+        } else {
+            die("ERROR in ArrayList.remove <br> Integer value required");
         }
+    }
+    public function clear()
+    {
+        $this->elements = [];
+    }
+    public function isEmpty()
+    {
+        return count($this->elements)==0;
+    }
+    public function sort()
+    {
+        sort($this->elements);
+    }
+    public function size()
+    {
+        return count($this->elements);
+    }
+    public function reset()
+    {
+        reset ($this->elements);
     }
 }
