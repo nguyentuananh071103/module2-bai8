34d826a0b61abb057db7fc66cf61d49dc1d2d609
nguyentuananh071103 <nguyenngocbao13579@gmail.com>
2021-11-02 21:13:17 +0700

    update

---
+++ quan li diem sinh vien/Student.php
@@ -0,0 +1,26 @@
+<?php
+class Student
+{
+    public $name;
+    public $score;
+    public $next;
+
+    /**
+     * @param $name
+     * @param $score
+     * @param $next
+     */
+    public function __construct($name, $score)
+    {
+        $this->name = $name;
+        $this->score = $score;
+        $this->next = null;
+    }
+
+    public function readStudent()
+    {
+        return $this->name." ".$this->score;
+    }
+
+
+}
